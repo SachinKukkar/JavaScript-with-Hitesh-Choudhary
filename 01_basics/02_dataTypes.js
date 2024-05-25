2659e091855594d8ccfcfbce2cb9c3014c5748c8
Sachin Kukkar <119757438+SachinKukkar@users.noreply.github.com>
2024-05-25 03:17:16 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" ; // treat all JS code as newer version code
+
+// alert(3+3)  // we are using NodeJs, not browser , isliye kaam nhi krega ye aise
+
+// Keep in mind that your code readability should be high
+
+let name = "Sachin"
+let age = 20
+let isLoggedin = false
+let state;
+
+// number => 2 to the power 53 tk range hota hai 
+// bigint => used for very very large numbers 
+// string => ""
+// boolean => true/false
+
+// null => standalone value hai and datatype bhi hai || null is representation of an empty value 
+
+// undefined => jab value define nhi kari hai , 
+
+// symbol => used for uniqueness 
+
+// object =>
+
+    console.log(typeof null); // output is object - interview question hai
+    console.log(typeof undefined); // output is undefined 
