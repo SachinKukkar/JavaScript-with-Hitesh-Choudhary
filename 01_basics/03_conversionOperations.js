a6f6b26adcb6fb1b9e8595811542fafffd4f92ed
Sachin Kukkar <119757438+SachinKukkar@users.noreply.github.com>
2024-05-25 03:34:50 +0000

    Conversion of Data Types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score = "33abc"
+
+console.log(typeof score);
+
+console.log(typeof (score));
+
+// upr wale dono hi tarike sahi hai 
+
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber));
+console.log(valueInNumber);
+
+
+// "33" => easily convert ho jaega number 33 me 
+// "33abc" => NaN (Not a Number)
+// null => 0 aega
+// bool => 0/1 aega as per false/true
+// undefined => NaN hi aega
+
+
+/* Note the the type of NaN is "number" */
+
+let isLoggedIn = 1 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let someNum = 33
+let stringNum = String(someNum)
+console.log(stringNum)
