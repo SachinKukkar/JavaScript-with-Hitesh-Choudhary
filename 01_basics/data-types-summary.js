dab8147ad9615b85069e50d4a07d10ea72d81ab7
Sachin Kukkar <119757438+SachinKukkar@users.noreply.github.com>
2024-06-01 12:21:39 +0000

    Data Types summary done

---
+++ 01_basics/data-types-summary.js
@@ -0,0 +1,32 @@
+// Primitive Data Types : ye call by value hote hai , copy krke alag dia jata hai original me koi change nhi hota
+
+// 7 Types : String , number , boolean , null , undefined , symbol  , BigInt
+
+// Reference type or non - primitive data types
+// Types : Arrays , Objects , Funtions 
+
+// JavaScript is statically typed or dynamically types ?? - HW 
+
+// const score = 100
+// const scoreValue = 100.3
+// const isLoggedIn = false
+// const Temperature = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const another_id = Symbol('123')
+
+// console.log(id===another_id);
+
+
+const hero = ["Shaktiman","Naagraj","Chacha-Choudhary"];
+let myObj = {
+    name:"Sachin",
+    age:21,
+}
+
+
+const myMyfunction = function() {
+    console.log("hello ji kya haal chaal aap sabhi ke !!!");
+}
+
