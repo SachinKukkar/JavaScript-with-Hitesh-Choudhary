b3275e48779628634601e7f59d7436b7c71ad886
Sachin Kukkar <119757438+SachinKukkar@users.noreply.github.com>
2024-06-01 12:08:08 +0000

    comparisons done

---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+
+console.log(null > 0 );
+console.log(null == 0 );
+console.log(null >= 0 );
+
+// same pange aenge with "undefined" , but hamesha ye false dega 
+
+// === is used to compare strictly 
+console.log("2"===2);
+
+// isiliye aise comparison nhin karne hai 
